c04c4dbf8955e8c95fbb00c9168b8c2c33499c71
alexander-muii <113370379+alexander-muii@users.noreply.github.com>
2022-11-10 12:48:25 -0500

    start functions section in java 💯

---
+++ src/Functions.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Functions
+{
+    public static void main(String[] args)
+    {
+        // reutilizar este codigo a travez de una función
+        System.out.println("Hola mundo!");
+        System.out.println("Hola mundo!");
+    }
+}
